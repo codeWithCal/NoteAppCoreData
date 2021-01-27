7d2e13eb1cae5b5ac9092d96936bb9b7dfcbad46
Callum Hill <ccahill.is@gmail.com>
2021-01-27 12:38:40 +1100

    Initial Commit

---
+++ NoteAppCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NoteAppCoreData
+//
+//  Created by CallumHill on 27/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
